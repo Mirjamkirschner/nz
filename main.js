0b67e064f74918eb34de01c47498347f6d321160
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-03-30 15:52:45 +0200

    variable title und zoom

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-var map = L.map('map').setView([-45.345275, 170.826061], 13);
+let stop_lat = -45.345275;
+let stop_lng = 170.826061;
+let zoom = 13;
+let title = 'Moeraki Boulders'
+
+
+let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-45.345275, 170.826061]).addTo(map)
-    .bindPopup('Moeraki Boulders.')
+L.marker([stop_lat, stop_lng]).addTo(map)
+    .bindPopup(title)
     .openPopup();
