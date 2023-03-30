5010c68388eae1b2f3f8485aacee23c3a1b33b9d
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-03-30 16:43:12 +0200

    for of Schleife für die STOPS eingefügt

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ const STOPS = [
         wikipedia: "https://de.wikipedia.org/wiki/Auckland"
     }];
 
-console.log(STOPS);
+
 
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
@@ -95,3 +95,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([stop_lat, stop_lng]).addTo(map)
     .bindPopup(title)
     .openPopup();
+
+    for (let stop of STOPS) {
+        console.log(stop);
+        console.log(stop.title);
+        console.log(stop.user);
+        console.log(stop.lat);
+        console.log(stop.lng);
+        console.log(stop.wikipedia);
+    }
