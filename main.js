7162977816711408164a1a62fec9928a1794cfc2
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-03-30 16:47:44 +0200

    marker für allle etappen in die karte eingefügt (in die for-Schleife)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let stop_lat = -45.345275;
 let stop_lng = 170.826061;
-let zoom = 13;
+let zoom = 6;
 let title = 'Moeraki Boulders'
 
 const STOPS = [
@@ -92,10 +92,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([stop_lat, stop_lng]).addTo(map)
-    .bindPopup(title)
-    .openPopup();
-
     for (let stop of STOPS) {
         console.log(stop);
         console.log(stop.title);
@@ -103,4 +99,8 @@ L.marker([stop_lat, stop_lng]).addTo(map)
         console.log(stop.lat);
         console.log(stop.lng);
         console.log(stop.wikipedia);
+    
+        L.marker([stop.lat, stop.lng]).addTo(map)
+        .bindPopup(stop.title)
+        .openPopup();
     }
